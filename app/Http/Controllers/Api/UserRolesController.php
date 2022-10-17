6ddd61a97ffbfd08df314633b06a9fdd006bfe53
ovedfs <ovedfs@gmail.com>
2022-10-17 14:33:49 -0500

    Agregamos la funcionalidad para vincular Roles con Usuarios

---
+++ app/Http/Controllers/Api/UserRolesController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Spatie\Permission\Models\Role;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+
+class UserRolesController extends Controller
+{
+    public function show(User $user)
+    {
+        $roles = Role::all();
+
+        return response()->json([
+            'user' => $user->load('roles'),
+            'roles' => $roles
+        ]);
+    }
+
+    public function sync(Request $request, User $user)
+    {
+        $roles = Role::all();
+        $rolesIdsArray = $roles->pluck('id')->toArray();
+
+        Validator::make($request->all(), [
+            "roles"   => ["sometimes", "array", Rule::in($rolesIdsArray)],
+            'roles.*' => 'sometimes|integer|distinct',
+        ])->validate();
+
+        $user->roles()->sync($request->roles);
+
+        return response()->json([
+            'user' => $user->load('roles')
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\RoleController;
+use App\Http\Controllers\Api\UserRolesController;
 use App\Http\Controllers\Api\PermissionController;
 use App\Http\Controllers\Api\RolePermissionsController;
 
@@ -15,9 +16,17 @@ Route::group(['middleware' => ["auth:sanctum"]], function(){
     Route::get('profile', [AuthController::class, 'profile']);
     Route::get('logout', [AuthController::class, 'logout']);
     
+    // CRUD Permissions
     Route::apiResource('permissions', PermissionController::class);
+
+    // CRUD Roles
     Route::apiResource('roles', RoleController::class);
 
+    // Vincular Permisos a Roles
     Route::get('role/{role}/permissions', [RolePermissionsController::class, 'show']);
     Route::post('role/{role}/permissions', [RolePermissionsController::class, 'sync']);
+
+    // Vincular Roles a Usuarios
+    Route::get('user/{user}/roles', [UserRolesController::class, 'show']);
+    Route::post('user/{user}/roles', [UserRolesController::class, 'sync']);
 });
